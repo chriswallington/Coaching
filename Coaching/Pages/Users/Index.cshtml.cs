e3ab6145cc6b67b12a4eee0d03fcbba4da1263c7
Chris Wallington <chris@carpedatum.co.uk>
2020-04-09 16:37:53 +0100

    Start of User registration/sign in

---
+++ Coaching/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Coaching.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coaching.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<UserModel> _userManager;
+        private readonly SignInManager<UserModel> _signInManager;
+
+        public IndexModel(UserManager<UserModel> userManager, SignInManager<UserModel> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+    
+    }
+}
